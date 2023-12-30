89428942b6d7ddee138bb43c276870ef92b706a5
Scripter1978 <scott.l.griffin@gmail.com>
2023-12-29 22:17:31 -0500

    Got SupaBase Query to work

--- HtmxBlazor/Program.cs
+++ HtmxBlazor/Program.cs
@@ -1,5 +1,6 @@
 using Htmx.TagHelpers;
 using HtmxBlazor.Components;
+using Infrastructure.Services.Public;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,8 +8,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
-var app = builder.Build();
+builder.Services.AddScoped<IPublicService, PublicService>();
+// ---------- SUPABASE
+var url = "https://yjhcoittdmimcaxtffer.supabase.co";
+var key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqaGNvaXR0ZG1pbWNheHRmZmVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDMwMjU4MTUsImV4cCI6MjAxODYwMTgxNX0.6zH8NNxUhDL_thuLK_xyHMzY6eHQfbiz54z_VohEgOs";
 
+builder.Services.AddScoped<Supabase.Client>(
+    provider => new Supabase.Client(
+        url,
+        key,
+        new Supabase.SupabaseOptions
+        {
+            AutoRefreshToken = true,
+            AutoConnectRealtime = true
+        }
+    )
+);
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
